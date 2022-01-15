bd9425d37df6178770ee6ffe8f5040d8260025ee
Banstein <islambahnas1@gmail.com>
2022-01-15 02:25:03 +0200

    testing

--- src/index.js
+++ src/index.js
@@ -1,41 +1,42 @@
 import './style.css';
 
+// variables
 const taskInject = document.querySelector('.task-injector');
+const taskInput = document.querySelector('.task-input');
+const addBtn = document.querySelector('#add-sign');
+const dltBtn = document.querySelectorAll('.task-delete');
+let listData = [];
 
-const listData = ([
-  {
-    description: 'Module 1 : w1 ~ w5',
-    completed: true,
-    index: 0,
-  },
-  {
-    description: 'Module 2 : w2 ~ w5',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'Module 3',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Module 4',
-    completed: false,
-    index: 1,
-  },
-]);
+// render
+const render = (items, inject) => {
+  inject.innerHTML = '';
+  items.forEach((data) => {
+    items.sort((a, b) => a.index - b.index);
+    inject.innerHTML += `
+    <li id="${data.index}">
+    <div class="task-wrapper">
+    <input type="checkbox" id="task-check" name="${data.index}" ${data.completed ? 'checked' : ''}>
+    <label class="dat-task ${data.completed ? 'line-through' : ''}" for="${data.index}">${data.description}</label>
+    </div>
+    <div>
+    <button class="task-edit"><i class="far fa-edit"></i></button>
+    <button class="task-delete"><i class="far fa-trash-alt"></i></button>
+    </div>
+    </li>
+  `;
+  });
+  dltBtn.style.visibility = 'hidden';
+};
 
-listData.forEach((data) => {
-  listData.sort((a, b) => a.index - b.index);
-  taskInject.innerHTML += `
-  <li>
-  <div class="task-wrapper">
-  <input type="checkbox" id="task-check" name="${data.index}" ${data.completed ? 'checked' : ''
-}>
-  <label class="dat-task ${data.completed ? 'line-through' : ''}" for="${data.index
-}">${data.description}</label>
-  </div>
-  <i class="fas fa-ellipsis-v" id="task-toggle"></i>
-</li>
-`;
+// add task
+addBtn.addEventListener('click', () => {
+  const task = {
+    index: listData.length,
+    description: taskInput.value,
+    completed: false,
+  };
+  listData = [...listData, task];
+  render(listData, taskInject);
 });
+
+// delete task
