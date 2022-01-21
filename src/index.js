4b51178c09d1d33107a3093608611986e1521fbb
Banstein <islambahnas1@gmail.com>
2022-01-21 15:30:08 +0200

    Update JS & HTML, Add CRUD functions

--- src/index.js
+++ src/index.js
@@ -1,42 +1,113 @@
 import './style.css';
+import { addTask } from './Modules/add.js';
 
 // variables
+let list = JSON.parse(localStorage.getItem('storedStTask')) || [];
 const taskInject = document.querySelector('.task-injector');
 const taskInput = document.querySelector('.task-input');
 const addBtn = document.querySelector('#add-sign');
-const dltBtn = document.querySelectorAll('.task-delete');
-let listData = [];
 
 // render
-const render = (items, inject) => {
-  inject.innerHTML = '';
-  items.forEach((data) => {
-    items.sort((a, b) => a.index - b.index);
-    inject.innerHTML += `
-    <li id="${data.index}">
-    <div class="task-wrapper">
-    <input type="checkbox" id="task-check" name="${data.index}" ${data.completed ? 'checked' : ''}>
-    <label class="dat-task ${data.completed ? 'line-through' : ''}" for="${data.index}">${data.description}</label>
-    </div>
-    <div>
-    <button class="task-edit"><i class="far fa-edit"></i></button>
-    <button class="task-delete"><i class="far fa-trash-alt"></i></button>
-    </div>
-    </li>
-  `;
+class Task {
+  constructor(value) {
+    this.description = value;
+    this.completed = false;
+    this.index = list.length + 1;
+  }
+}
+
+// display elements
+const displayTasks = (tasks) => {
+  taskInject.innerHTML = '';
+  tasks.forEach((task) => {
+    const li = document.createElement('li');
+    li.classList.add('li');
+    li.setAttribute('id', task.index);
+    taskInject.appendChild(li);
+    //
+    const span = document.createElement('span');
+    span.classList.add('task-wrapper');
+    li.appendChild(span);
+    //
+    const check = document.createElement('input');
+    check.classList.add('task-check');
+    check.type = 'checkbox';
+    span.appendChild(check);
+    //
+    const label = document.createElement('label');
+    label.classList.add('dat-task');
+    label.innerText = task.description;
+    span.appendChild(label);
+    //
+    const div = document.createElement('div');
+    li.appendChild(div);
+    //
+    const edtBtn = document.createElement('button');
+    div.appendChild(edtBtn);
+    //
+    const dltBtn = document.createElement('button');
+    div.appendChild(dltBtn);
+    //
+    const iDlt = document.createElement('i');
+    iDlt.classList.add('task-delete', 'far', 'fa-trash-alt');
+    dltBtn.appendChild(iDlt);
+    //
+    const iEdt = document.createElement('i');
+    iEdt.classList.add('task-edit', 'far', 'fa-edit');
+    edtBtn.appendChild(iEdt);
   });
-  dltBtn.style.visibility = 'hidden';
 };
 
-// add task
-addBtn.addEventListener('click', () => {
-  const task = {
-    index: listData.length,
-    description: taskInput.value,
-    completed: false,
-  };
-  listData = [...listData, task];
-  render(listData, taskInject);
+// update
+function UpdateStorage(e) {
+  localStorage.setItem('storedStTask', JSON.stringify(e));
+}
+// add
+const add = () => {
+  if (taskInput.value.trim() !== '') {
+    const newTask = new Task(taskInput.value);
+    addTask(list, newTask);
+    UpdateStorage(list);
+    displayTasks(list);
+    taskInput.value = '';
+    taskInput.placeholder = 'Add another Task!';
+  } else { taskInput.placeholder = 'Please enter a valid Task!'; }
+};
+
+addBtn.addEventListener('click', add);
+taskInput.addEventListener('keypress', (event) => {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    add();
+  }
 });
 
-// delete task
+document.addEventListener('click', (e) => {
+  if (e.target.className === 'task-delete far fa-trash-alt') {
+    const eLi = e.target.parentNode.parentNode.parentNode;
+    list = list.filter((item) => item.index !== Number(eLi.id));
+    list.forEach((item, i) => {
+      item.index = i + 1;
+    });
+    UpdateStorage(list);
+    displayTasks(list);
+  }
+});
+
+document.addEventListener('click', (e) => {
+  if (e.target.className === 'task-edit far fa-edit') {
+    const editer = prompt('Renew Task!');
+    const eLi = e.target.parentNode.parentNode.parentNode;
+    list.forEach((item) => {
+      if (editer.length > 1 && item.index === Number(eLi.id)) {
+        item.description = editer;
+      }
+    });
+    UpdateStorage(list);
+    displayTasks(list);
+  }
+});
+
+window.onload = () => {
+  displayTasks(list);
+};
