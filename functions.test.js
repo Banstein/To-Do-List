96dfebb4fdc1b2be328bbd91967921e1c6e6ffda
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-02-24 08:47:40 +0000

    add test for local storage

--- functions.test.js
+++ functions.test.js
@@ -1,7 +1,32 @@
-import UpdateStorage from './src/Modules/updateStorage.js';
-import addTask from './src/Modules/add.js';
+// import UpdateStorage from './src/Modules/updateStorage.js';
+// import addTask from './src/Modules/add.js';
 
 import LocalStorage from './__mocks__/localStorage.js';
-import domMock from './__mocks__/domMock.js';
+// import domMock from './__mocks__/domMock.js';
 
-global.localStorage = new LocalStorage();
+// global.localStorage = new LocalStorage();
+
+describe('test localStorage', () => {
+  it('Adds an item to the mock localStorage', () => {
+    const localStorage = new LocalStorage();
+    const key = 'mockKey';
+    const value = 'storageValue';
+
+    localStorage.setItem(key, value);
+
+    expect(
+      Object.keys(localStorage.storage).length,
+    ).toEqual(1);
+  });
+
+  it('gets the items from the mock localStorage', () => {
+    const localStorage = new LocalStorage();
+    const key = 'mockKey';
+    const value = 'storageValue';
+    localStorage.setItem(key, value);
+
+    const receivedValue = localStorage.getItem(key);
+
+    expect(receivedValue).toEqual(value);
+  });
+});
