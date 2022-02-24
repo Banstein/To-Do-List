bf3a070ea03b8775a8f7a987a0054c48f46f9870
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-02-24 08:31:51 +0000

    Update the function.test.js

--- functions.test.js
+++
@@ -1,6 +0,0 @@
-import UpdateStorage from './src/Modules/updateStorage.js';
-import addTask from './src/Modules/add.js';
-
-import LocalStorage from './__mocks__/localStorage.js';
-
-global.localStorage = new LocalStorage();
