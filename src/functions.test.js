ecd55f47d547136d396ff441b67ed1d364ac389c
Banstein <islambahnas1@gmail.com>
2022-02-24 10:08:57 +0200

    Update functions.test.js

--- src/functions.test.js
+++ src/functions.test.js
@@ -1,2 +1,6 @@
 import UpdateStorage from './Modules/updateStorage.js';
 import addTask from './Modules/add.js';
+
+import LocalStorage from './__mocks__/localStorage.js';
+
+global.localStorage = new LocalStorage();
