cb6dadfd457f79559f0ae2656cbbcc1e838948c7
Banstein <islambahnas1@gmail.com>
2022-02-24 10:15:53 +0200

    Update file, change location

--- src/functions.test.js
+++
@@ -1,6 +0,0 @@
-import UpdateStorage from './Modules/updateStorage.js';
-import addTask from './Modules/add.js';
-
-import LocalStorage from './__mocks__/localStorage.js';
-
-global.localStorage = new LocalStorage();
